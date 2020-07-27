e2e6bf54a5c33ac3f7d844d8a632776a153312d2
Tanay Tapanshu <tanaytapanshu.com>
2020-07-27 15:42:15 +0530

    React Insta clone Application

---
+++ build/precache-manifest.11d2ea1f9aeb5e61d04b1c51f67261a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9a28a8a585c618b8da44c8f439b2ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad4089fc0da0551e883",
+    "url": "/static/css/main.0481c783.chunk.css"
+  },
+  {
+    "revision": "852a66e2f5a678e1abe3",
+    "url": "/static/js/2.7ce187d5.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.7ce187d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad4089fc0da0551e883",
+    "url": "/static/js/main.869735f5.chunk.js"
+  },
+  {
+    "revision": "12ffa9c2a2e2bca05bc1",
+    "url": "/static/js/runtime-main.a41b20c0.js"
+  },
+  {
+    "revision": "d5ead2c53407dacd6c52a25549ca345f",
+    "url": "/static/media/I1.d5ead2c5.png"
+  },
+  {
+    "revision": "4231b85f476a3b03df328616efbd490d",
+    "url": "/static/media/dev.4231b85f.png"
+  }
+]);
